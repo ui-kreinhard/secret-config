03498755e2b2a829d7ff89bea0147d2e4ac640df
Karl-Ludwig Reinhard <reinhard@synyx.de>
2024-05-25 07:27:34 +0200

    Add Cache file

--- url-tag/url.go
+++ url-tag/url.go
@@ -1,10 +1,13 @@
 package urltag
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"reflect"
 	"runtime"
 	"syscall"
@@ -13,11 +16,61 @@ import (
 	"golang.org/x/term"
 )
 
+func isDevModeSet(envName string) bool {
+	_, isSet := os.LookupEnv(envName)
+	return isSet
+}
+
+func getCacheFile() string {
+	name, _ := os.Executable()
+	name = filepath.Base(name)
+	return filepath.Join("/tmp", name)
+}
+
+func checkForCacheFile() bool {
+	tmpFile := getCacheFile()
+	if _, err := os.Stat(tmpFile); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return true
+}
+
+func writeCacheFile(urlSecretMap map[string]string) error {
+	data, err := json.Marshal(urlSecretMap)
+	if err != nil {
+		return err
+	}
+	cacheFile := getCacheFile()
+	return os.WriteFile(cacheFile, data, 0644)
+}
+
+func readCacheFile() (map[string]string, error) {
+	ret := map[string]string{}
+	rawData, err := os.ReadFile(getCacheFile())
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(rawData, &ret)
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
 func ScanForUrlAndOpen[T any](object T, envName string) T {
 	urlSecretMap := make(map[string]string)
-	if _, isSet := os.LookupEnv(envName); !isSet {
+	if !isDevModeSet(envName) {
 		log.Println("env not set")
 		return object
+	}
+	if checkForCacheFile() {
+		cachedSecretMap, err := readCacheFile()
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println("Found cached file")
+			urlSecretMap = cachedSecretMap
+		}
 	}
 	rt := reflect.TypeOf(object)
 	rf := reflect.Indirect(reflect.ValueOf(&object))
@@ -47,6 +100,10 @@ func ScanForUrlAndOpen[T any](object T, envName string) T {
 			}
 			rf.Field(i).SetString(decrypted)
 		}
+	}
+	err := writeCacheFile(urlSecretMap)
+	if err != nil {
+		log.Println("Cannot write cache file", err)
 	}
 	return object
 }
